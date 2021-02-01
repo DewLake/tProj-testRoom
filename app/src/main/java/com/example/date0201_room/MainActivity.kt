1f0bf540cb51761624414833c4504a3d9586e895
mygit <myemail@mail.com>
2021-02-01 14:41:50 +0800

    init commit.

---
+++ app/src/main/java/com/example/date0201_room/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.date0201_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+} // end class MainActivity.
