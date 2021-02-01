58869b00badc6b9f9c4b7b7ac624b69c30a561da
mygit <myemail@mail.com>
2021-02-01 15:09:33 +0800

    ok, load fragment. prepare, layout.

--- app/src/main/java/com/example/date0201_room/MainActivity.kt
+++ app/src/main/java/com/example/date0201_room/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.date0201_room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.date0201_room.ui.BooksFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<BooksFragment>(R.id.framContainer__MainActivity, "TAG_BooksFragment")
+            }
+        }
     }
 } // end class MainActivity.
---
+++ app/src/main/java/com/example/date0201_room/ui/BooksFragment.kt
@@ -0,0 +1,23 @@
+package com.example.date0201_room.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.date0201_room.R
+
+class BooksFragment: Fragment(R.layout.fragment_books) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+} // end class BooksFragment.
