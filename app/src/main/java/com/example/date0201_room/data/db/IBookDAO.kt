2a5fc97282bd2b42f7bd7e7caf4567c55d0664a6
mygit <myemail@mail.com>
2021-02-05 11:54:17 +0800

    ok, prepare clean code.

--- app/src/main/java/com/example/date0201_room/data/db/IBookDAO.kt
+++ app/src/main/java/com/example/date0201_room/data/db/IBookDAO.kt
@@ -12,10 +12,10 @@ interface IBookDAO {
     @Query("SELECT * FROM ${Book.TABLE_NAME} ORDER BY id DESC")
     fun getAllBooks(): LiveData<List<Book>>
 
-//    @Query("SELECT * FROM table_books WHERE (:title IS NULL OR title Like '%' || :title || '%') OR (:price IS NULL OR price = :price) ORDER By id DESC ")
-//    fun getBooks(title: String? = null, price: Double? = null): List<Book>
-@Query("SELECT * FROM table_books WHERE (:title IS NULL OR title Like '%' || :title || '%') AND (:price IS NULL OR price = :price) ORDER By id DESC ")
-fun getBooks(title: String? = "u", price: Double? = 10.0): List<Book>
+    @Query("SELECT * FROM table_books WHERE (:title IS NULL OR title Like '%' || :title || '%') AND (:price IS NULL OR price = :price) ORDER By id DESC ")
+    fun getBooks(title: String? = null, price: Double? = null): List<Book>
+//@Query("SELECT * FROM table_books WHERE (:title IS NULL OR title Like '%' || :title || '%') AND (:price IS NULL OR price = :price) ORDER By id DESC ")
+//fun getBooks(title: String? = "u", price: Double? = 10.0): List<Book>
 
     @Query("SELECT * FROM ${Book.TABLE_NAME} WHERE id LIKE :id LIMIT 1")
     suspend fun getBookById(id: Long): Book
